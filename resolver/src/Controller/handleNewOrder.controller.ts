bac29a223dd34f19feeb0b20c8136b572fb5b863
Arunava-Barua <rahulbarua31@gmail.com>
2025-07-31 01:04:05 +0530

    Fixed new order

--- resolver/src/Controller/handleNewOrder.controller.ts
+++ resolver/src/Controller/handleNewOrder.controller.ts
@@ -5,7 +5,12 @@ export const handleNewOrder = async (req: Request, res: Response) => {
   try {
     // 1. Find a profitable deal
     // Calculation: Profit = ((marketPrice - auctionPrice) / auctionPrice) > MIN_PROFIT_MARGIN (also account gas fees)
-      // 1. Interact with Dutch Auction, 2. Deploy in sec, 3. Deploy in dest, 4. Add tokens in escrows, 5. Withdraw tokens
+      // 1. Interact with Dutch Auction, 
+      // 2. Deploy in sec, 
+      // 3. Deploy in dest, 
+      // 4. Add tokens in escrows, 
+      // 5. Withdraw tokens, 
+      // 6. Relayer Fees
     // 2. Accept a price in Dutch Auction smart contract
     // 3. Deploy a HTLC on source chain (ETH) with security deposit
     // 4. Deploy a HTLC on destination chain (Stellar) with security deposit
