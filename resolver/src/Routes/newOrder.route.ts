ab72afe0c9d6df24950016b6d76d05f8deaeb3b3
Arunava-Barua <rahulbarua31@gmail.com>
2025-07-31 00:48:49 +0530

    resolver flow

--- resolver/src/Routes/newOrder.route.ts
+++ resolver/src/Routes/newOrder.route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { handleNewOrder } from '../Controller/handleNewOrder.controller';
+import { handleOrderSecret } from '../Controller/handleOrderSecret.controller';
 
 const router = express.Router();
 
 // Handle new order
-router.post('/', handleNewOrder);
+router.post('/new-order', handleNewOrder);
+router.post('/secret', handleOrderSecret);
 
 
 export default router;
